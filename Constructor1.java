3f5ce42cb38a86f09c3a2be4a1b571b6ddbce180
PanzerG <82965256+PanzerG@users.noreply.github.com>
2021-10-30 16:51:32 +0300

    Add files via upload

---
+++ Constructor1.java
@@ -0,0 +1,20 @@
+
+package constructor1;
+
+
+public class Constructor1 {
+
+    
+    public static void main(String[] args) {
+       // Account account1 = new Account();
+        
+        Account account2 = new Account("Ahmetcan", "babalikumut1@gmail.com", "0123242034");
+        
+        account2.bilgileriGoster();
+        
+        
+        
+        
+    }
+    
+}
